5b1d6c1ba01b7dc5525bcf41abe24cfa5626d561
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-02 15:42:58 +0600

    Module #6. Task #1. Class creation

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,54 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    public static void main(String[] args) {
+        //Animal a = new Animal("green", 1, false);
+        //System.out.println(a.getDescription());
+
+        /*(Optional) Create an object of each class and call getDescription() method for both of them.
+        Try to explain the output results.*/
+        Dog dog = new Dog();
+        System.out.println(dog.getDescription());
+
+        Animal bird = new Bird();
+        System.out.println(bird.getDescription());
+
+    }
+
+    //Provide it with 3 private fields - color(String), numberOfPaws(int), hasFur(boolean).
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    //Add constructor with full parameters. Save the parameter order and names as it is listed in a second paragraph.
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    //Add a methods getDescription(),
+    //which would use class fields and return a string with such pattern "This animal is mostly (color).
+    //It has (numberOfPaws) paws and ('a'/'no' -> depends on value of hasFur) fur."
+
+    /*(Optional) In the method getDescription() change the word 'paw' depending on the numberOfPaws:
+    number of paws is 1 -> 'paw', number of paws is different from 1 -> 'paws'.*/
+    String getDescription() {
+        String fur = isHasFur() ? "a" : "no";
+        String paws = getNumberOfPaws() == 1 ? "paw" : "paws";
+        return "This animal is mostly " + getColor() + ". It has " + getNumberOfPaws() +
+                " " + paws + " and " + fur + " fur.";
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,31 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    private int wings = 2;
+    private boolean isFlying = true;
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    /*Override getDescription() method for class Bird: add one more sentence to the description.
+    The result must be:
+    "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly." */
+
+    @Override
+    String getDescription() {
+        String fur = isHasFur() ? "a" : "no";
+        String paws = getNumberOfPaws() == 1 ? "paw" : "paws";
+        String isFlying = isFlying() ? "can fly." : "cannot fly.";
+        return "This animal is mostly " + getColor() + ". It has " + getNumberOfPaws() +
+                " " + paws + " and " + fur + " fur. Moreover, it has " + getWings() + " wings and " + isFlying;
+    }
+
+    public int getWings() {
+        return wings;
+    }
+
+    public boolean isFlying() {
+        return isFlying;
+    }
 }
