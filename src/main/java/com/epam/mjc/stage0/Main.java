a7bbcd9aa951caa61b04a108eea284b638e692ce
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-02 17:45:33 +0600

    Module #6. Task #1. Class creation v4

--- src/main/java/com/epam/mjc/stage0/Main.java
+++ src/main/java/com/epam/mjc/stage0/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         /*(Optional) Create an object of each class and call getDescription() method for both of them.
         Try to explain the output results.*/
+        Animal animal = new Animal("red", 1, true);
+        System.out.println(animal.getDescription());
 
         Dog dog = new Dog();
         System.out.println(dog.getDescription());
